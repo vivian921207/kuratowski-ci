d55d957842dde5e7609b12790a2aa404736e5579
vivian921207 <lf2.freeze@gmail.com>
2025-04-08 05:04:19 +0800

    a fail test

--- Calc.py
+++ Calc.py
@@ -14,6 +14,7 @@ class Calculator:
         return a * b
 
     def divide(self, a, b):
+        return a / b
         """回傳 a / b，如果 b 為 0 則拋出錯誤"""
         if b == 0:
             raise ValueError("Cannot divide by zero")
