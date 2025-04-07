af94f8bfa06cb078c9185ad75ca34b6a2c3e53c7
vivian921207 <lf2.freeze@gmail.com>
2025-04-08 04:55:41 +0800

    新增報告與圖片

---
+++ Calc.py
@@ -0,0 +1,20 @@
+# Calc.py
+
+class Calculator:
+    def add(self, a, b):
+        """回傳 a + b"""
+        return a + b
+
+    def subtract(self, a, b):
+        """回傳 a - b"""
+        return a - b
+
+    def multiply(self, a, b):
+        """回傳 a * b"""
+        return a * b
+
+    def divide(self, a, b):
+        """回傳 a / b，如果 b 為 0 則拋出錯誤"""
+        if b == 0:
+            raise ValueError("Cannot divide by zero")
+        return int (a / b)
